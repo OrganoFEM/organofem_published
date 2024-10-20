466bb691838c42b59e45ee63ab65b5bc24ad5742
Laussu Julien <56252845+EptmSIM@users.noreply.github.com>
2024-10-20 16:44:36 +0200

    Add files via upload

---
+++ version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM organosim SETUP.PY
+#
+short_version = '0.0.1'
+full_version = '0.0.1'
+git_revision = 'Unknown'
+release = True
+if release:
+    version = full_version
+else:
+    version = short_version
